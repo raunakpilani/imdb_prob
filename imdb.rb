e5a3d3ac93e179b96c8f715afab8f25aa139bc01
Raunak Pilani <raunak@browserstack.com>
2013-08-27 15:07:51 +0530

    correctly replace tabs with spaces

--- imdb.rb
+++ imdb.rb
@@ -3,9 +3,9 @@ require 'open-uri'
 
 class Imdb 
   def initialize(link)
-  	@main_page = link
-  	@movie_links_hash = Hash.new
-  	@movie_cast_hash = Hash.new
+    @main_page = link
+    @movie_links_hash = Hash.new
+    @movie_cast_hash = Hash.new
   end
 
   attr_reader :main_page
@@ -13,108 +13,108 @@ class Imdb
   attr_accessor :movie_cast_hash
 
   def html_parse_with_xpath(url,path)
-  	Nokogiri::HTML.parse(open(url)).xpath(path)
+    Nokogiri::HTML.parse(open(url)).xpath(path)
   end
 
   def get_rank_range(rank_range)
-  	rank_arr = rank_range.split(":").collect { |m| m.to_i }
-  	rank_arr.push(1) until rank_arr.length == 2
-  	rank_arr.pop until rank_arr.length == 2
-  	rank_arr.sort!
-  	rank_arr = rank_arr.map { |r| r <= 0 ? 1 : r }
-  	rank_arr
+    rank_arr = rank_range.split(":").collect { |m| m.to_i }
+    rank_arr.push(1) until rank_arr.length == 2
+    rank_arr.pop until rank_arr.length == 2
+    rank_arr.sort!
+    rank_arr = rank_arr.map { |r| r <= 0 ? 1 : r }
+    rank_arr
   end
 
   def populate_link_hash
-  	top_250_line = html_parse_with_xpath(main_page,"//div[@id='navbar']/div/ul/li/ul/li/a").detect do |tagline| 
-  		tagline.content == "Top 250" 
-  	end
+    top_250_line = html_parse_with_xpath(main_page,"//div[@id='navbar']/div/ul/li/ul/li/a").detect do |tagline| 
+      tagline.content == "Top 250" 
+    end
 
-  	top_250_link = main_page + top_250_line['href']
+    top_250_link = main_page + top_250_line['href']
 
-  	rank_range = gets_with_message "Enter range of rank"
+    rank_range = gets_with_message "Enter range of rank"
 
-  	min_rank, max_rank = get_rank_range(rank_range)
+    min_rank, max_rank = get_rank_range(rank_range)
 
-  	puts "Displaying: #{min_rank} to #{max_rank}"
-  	min_rank -= 1
-  	curr_rank = min_rank
-  	html_parse_with_xpath(top_250_link,"//table[2]/tr/td/font/a")[min_rank...max_rank].each do |elem| 
-  		@movie_links_hash[curr_rank] = [elem.content,elem['href']]
-  		curr_rank += 1
-  	end
-  	@movie_links_hash.each { |k,v| puts "#{k+1}. #{v[0]}"}
+    puts "Displaying: #{min_rank} to #{max_rank}"
+    min_rank -= 1
+    curr_rank = min_rank
+    html_parse_with_xpath(top_250_link,"//table[2]/tr/td/font/a")[min_rank...max_rank].each do |elem| 
+      @movie_links_hash[curr_rank] = [elem.content,elem['href']]
+      curr_rank += 1
+    end
+    @movie_links_hash.each { |k,v| puts "#{k+1}. #{v[0]}"}
   end
 
   def populate_cast_hash
-  	@movie_links_hash.each do |k,v|
-  		cast_page = @main_page + v[1] + "fullcredits?ref_=tt_cl_sm#cast"
-  		cast_list = []
-  		html_parse_with_xpath(cast_page,"//table[@class='cast']/tr/td[@class='nm']/a").each do |cast_link| 
-  			cast_list.push(cast_link.content)
-  		end
-  		@movie_links_hash[k] = v[0] # changing movie_links hash to Rank => Movie
-  		@movie_cast_hash[k] = cast_list
-  	end
+    @movie_links_hash.each do |k,v|
+      cast_page = @main_page + v[1] + "fullcredits?ref_=tt_cl_sm#cast"
+      cast_list = []
+      html_parse_with_xpath(cast_page,"//table[@class='cast']/tr/td[@class='nm']/a").each do |cast_link| 
+        cast_list.push(cast_link.content)
+      end
+      @movie_links_hash[k] = v[0] # changing movie_links hash to Rank => Movie
+      @movie_cast_hash[k] = cast_list
+    end
   end
 
   def menu_option_m
-  	mov = gets_with_message "Exact name of Movie"
-  	rank = @movie_links_hash.key(mov)
-  	if rank 
-  		puts_movie_details(rank,mov)
-  	else 
-  		puts "Movie with this name doesnt exist!"
-  	end 
-  	true
+    mov = gets_with_message "Exact name of Movie"
+    rank = @movie_links_hash.key(mov)
+    if rank 
+      puts_movie_details(rank,mov)
+    else 
+      puts "Movie with this name doesnt exist!"
+    end 
+    true
   end
 
   def menu_option_r
-  	rank = gets_with_message "Enter rank of movie: "
-  	rank = rank.to_i
-  	rank -= 1
-  	if @movie_links_hash.has_key?(rank) 
-  		movie = @movie_links_hash[rank]
-  		puts_movie_details(rank,movie)
-  	else 
-  		puts "Movie with this rank doesnt exist"
-  	end 
-  	true
+    rank = gets_with_message "Enter rank of movie: "
+    rank = rank.to_i
+    rank -= 1
+    if @movie_links_hash.has_key?(rank) 
+      movie = @movie_links_hash[rank]
+      puts_movie_details(rank,movie)
+    else 
+      puts "Movie with this rank doesnt exist"
+    end 
+    true
   end
   
   def menu_option_c
-  	print "Enter name of cast member: "
-  	cast = STDIN.gets.chomp
+    print "Enter name of cast member: "
+    cast = STDIN.gets.chomp
 
-  	worked = false
+    worked = false
 
-  	output = "\t#{cast} has worked in: \n"
-  	@movie_cast_hash.each do |k,v|
-  		if v.include?(cast)
-  			output += "\t" + @movie_links_hash[k] 
-  			worked = true
-  		end
-  	end 
-  	STDOUT.puts output
-  	puts "Nothing!" unless worked
-  	true
+    output = "\t#{cast} has worked in: \n"
+    @movie_cast_hash.each do |k,v|
+      if v.include?(cast)
+        output += "\t" + @movie_links_hash[k] 
+        worked = true
+      end
+    end 
+    STDOUT.puts output
+    puts "Nothing!" unless worked
+    true
   end
   
   def menu_option_x
-  	STDOUT.puts "Goodbye!"
-  	false
+    STDOUT.puts "Goodbye!"
+    false
   end
 
   def puts_movie_details(rank, movie)
-  	STDOUT.puts "\tThe movie #{movie} has rank: #{rank+1}"
-  	output = "\tThe cast members are as follows: \n"
-  	@movie_cast_hash[rank].each { |cast_member| output += "\t" + cast_member }
-  	STDOUT.puts output
+    STDOUT.puts "\tThe movie #{movie} has rank: #{rank+1}"
+    output = "\tThe cast members are as follows: \n"
+    @movie_cast_hash[rank].each { |cast_member| output += "\t" + cast_member }
+    STDOUT.puts output
   end
 
   def gets_with_message(message)
-  	print message + " >"
-  	STDIN.gets.chomp
+    print message + " >"
+    STDIN.gets.chomp
   end
 end
 
@@ -138,9 +138,9 @@ begin
   print "Your choice[m/r/c/x]? "
   choice = gets.chomp
   begin
-  	continue = imdb.send "menu_option_"+choice
+    continue = imdb.send "menu_option_"+choice
   rescue NoMethodError => msg
-  	puts "Incorrect option entered!"
-  	continue = true
+    puts "Incorrect option entered!"
+    continue = true
   end
 end while continue
--- imdb_spec.rb
+++ imdb_spec.rb
@@ -2,70 +2,70 @@ require './spec_helper'
 require './imdb'
 
 describe Imdb do
-	before :each do
-		@imdb = Imdb.new "http://www.imdb.com"
-	end
-	describe "#new" do
-		it "returns instance" do
-			@imdb.should be_an_instance_of Imdb
-		end
-		it "raises argument error for no arguments" do
-			lambda { Imdb.new }.should raise_exception ArgumentError
-		end
-	end
-	describe "#html_parse_with_xpath" do
-		it "returns object of type xpath" do
-			x = @imdb.html_parse_with_xpath(@imdb.main_page,"//a")
-			x.should be_an_instance_of Nokogiri::XML::NodeSet
-		end
-	end
-	describe "#get_rank_range" do
-		it "returns a array with two integers, sorted, with a minimum value of 1" do
-			x,y = @imdb.get_rank_range("-1:a")
-			x.should be_an_instance_of Fixnum
-			y.should be_an_instance_of Fixnum
-			x.should be <= y
-			x.should be >= 1
-			y.should be >= 1
-		end
-	end
-	describe "#populate_link_hash" do
-		it "makes at least one entry in the link hash in the format rank => [name, path]" do
-			@imdb.populate_link_hash
-			h = @imdb.instance_variable_get(:@movie_links_hash)
-			k,v = h.first
-			k.should be_an_instance_of Fixnum
-			v.should be_an_instance_of Array
-			v.length.should be == 2
-			v[0].should be_an_instance_of String
-			v[1].should be_an_instance_of String
-		end
-	end
-	describe "#populate_cast_hash" do
-		it "makes at least one entry in the cast hash as rank => [member1,member2...]" do
-			@imdb.populate_link_hash
-			@imdb.populate_cast_hash
-			h = @imdb.instance_variable_get(:@movie_cast_hash)
-			k,v = h.first
-			k.should be_an_instance_of Fixnum
-			v.should be_an_instance_of Array
-			v.each do |val| 
-				val.should be_an_instance_of String
-			end
-		end
-	end
-	describe "#menu_option_m" do
-		it "asks for movie name and displays it's cast members" do
-			@imdb.populate_link_hash
-			@imdb.populate_cast_hash
+  before :each do
+    @imdb = Imdb.new "http://www.imdb.com"
+  end
+  describe "#new" do
+    it "returns instance" do
+      @imdb.should be_an_instance_of Imdb
+    end
+    it "raises argument error for no arguments" do
+      lambda { Imdb.new }.should raise_exception ArgumentError
+    end
+  end
+  describe "#html_parse_with_xpath" do
+    it "returns object of type xpath" do
+      x = @imdb.html_parse_with_xpath(@imdb.main_page,"//a")
+      x.should be_an_instance_of Nokogiri::XML::NodeSet
+    end
+  end
+  describe "#get_rank_range" do
+    it "returns a array with two integers, sorted, with a minimum value of 1" do
+      x,y = @imdb.get_rank_range("-1:a")
+      x.should be_an_instance_of Fixnum
+      y.should be_an_instance_of Fixnum
+      x.should be <= y
+      x.should be >= 1
+      y.should be >= 1
+    end
+  end
+  describe "#populate_link_hash" do
+    it "makes at least one entry in the link hash in the format rank => [name, path]" do
+      @imdb.populate_link_hash
+      h = @imdb.instance_variable_get(:@movie_links_hash)
+      k,v = h.first
+      k.should be_an_instance_of Fixnum
+      v.should be_an_instance_of Array
+      v.length.should be == 2
+      v[0].should be_an_instance_of String
+      v[1].should be_an_instance_of String
+    end
+  end
+  describe "#populate_cast_hash" do
+    it "makes at least one entry in the cast hash as rank => [member1,member2...]" do
+      @imdb.populate_link_hash
+      @imdb.populate_cast_hash
+      h = @imdb.instance_variable_get(:@movie_cast_hash)
+      k,v = h.first
+      k.should be_an_instance_of Fixnum
+      v.should be_an_instance_of Array
+      v.each do |val| 
+        val.should be_an_instance_of String
+      end
+    end
+  end
+  describe "#menu_option_m" do
+    it "asks for movie name and displays it's cast members" do
+      @imdb.populate_link_hash
+      @imdb.populate_cast_hash
       Imdb.any_instance.should_receive(:gets_with_message).with("Exact name of Movie").and_return("The Shawshank Redemption")
       Imdb.any_instance.should_receive(:puts_movie_details).with(1,"The Shawshank Redemption")
       @imdb.menu_option_m
-		end
-	end
-	describe "#puts_movie_details" do
-		it "raises ArgumentError for less than two arguments" do
-			lambda { @imdb.puts_movie_details }.should raise_exception ArgumentError
-		end
-	end
+    end
+  end
+  describe "#puts_movie_details" do
+    it "raises ArgumentError for less than two arguments" do
+      lambda { @imdb.puts_movie_details }.should raise_exception ArgumentError
+    end
+  end
 end
